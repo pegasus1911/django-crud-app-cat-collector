3be5d27abd3b055d305a34950f67e8e4d3e3ebbb
ali <alihasansaad1@gmail.com>
2025-08-21 10:19:36 +0300

    message describing code changes

--- main_app/views.py
+++ main_app/views.py
@@ -6,4 +6,4 @@ from django.http import HttpResponse
 def home(request):
     return HttpResponse('<h1>Hello </h1>')
 def about(request):
-    return HttpResponse('<h1>About the CatCollector</h1>')
+    return render(request,'about.html')
